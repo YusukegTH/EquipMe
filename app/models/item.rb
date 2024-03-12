0127b68dc4eb708edc5e61940fe01f8a9cbee6a8
gabi <gabrielteodorescu2212@yahoo.com>
2024-03-12 13:08:44 +0100

    Added methode to delete Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   attribute :address, :string
+
+  def delete_with_associations
+    bookings.destroy_all
+    destroy
+  end
 end
