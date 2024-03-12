27551ddab7033be4b1b99d696f0442708bd7936c
Yusuke Tomita Huuskonen <yusuke.g.tomita@gmail.com>
2024-03-12 14:41:19 +0100

    Devise implemented (columns added and parameters added)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :bookings
 
   validates :email, presence: true
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  has_many :items, dependent: :destroy
 end
