39c06b9145a0e464e296e922c8691518f86334f9
gabi <gabrielteodorescu2212@yahoo.com>
2024-03-12 16:13:11 +0100

    Bookings controller + routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :items, only: [:show, :new, :create]
+  resources :items, only: [:show, :new, :create] do
+    resources :bookings, except: [:show]
+  end
 end
