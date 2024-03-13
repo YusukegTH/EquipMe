237dfe4e113235065d90549361343df804795964
Joao Crespo <joao.crespo@live.com>
2024-03-13 14:21:30 +0100

    Updated

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
 
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def delete_with_associations
     bookings.destroy_all
