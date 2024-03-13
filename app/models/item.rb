7076b0b4bba732dfdc3ce5d705a1009083b7151a
Joao Crespo <joao.crespo@live.com>
2024-03-13 11:40:59 +0100

    geomap_feature updated

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,11 @@ class Item < ApplicationRecord
   has_many :bookings, dependent: :destroy
   attribute :address, :string
   has_one_attached :picture
-  
-   validates :name, presence: true
+
+  validates :name, presence: true
+
+  geocode_by :location
+  after_validation :geocode
 
   def delete_with_associations
     bookings.destroy_all
