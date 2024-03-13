1ebaad0f09d8548c0dcd85cb3ecb1a40d677149b
Joao Crespo <joao.crespo@live.com>
2024-03-13 15:13:00 +0100

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_113237) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
